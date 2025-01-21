3ce7c2181d73100e315923bd9666aef843b5a9cc
Mathrix <68941653+Dreams-Possible@users.noreply.github.com>
2025-01-21 19:34:15 +0800

    Add files via upload

---
+++ lvgl_port/lvgl_port.h
@@ -0,0 +1,14 @@
+//enable lvgl port
+#if 1
+#ifndef LVGL_PORT_H
+#define LVGL_PORT_H
+
+//include
+#include"lvgl/lvgl.h"
+#include"screen_driver.h"
+
+//lvgl port init
+void lvgl_port_init();
+
+#endif//#ifndef LVGL_PORT_H
+#endif//#if 1
